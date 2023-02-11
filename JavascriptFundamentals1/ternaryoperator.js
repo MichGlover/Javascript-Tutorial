a5a938afe20c617472c00aaf0883921950f9e9a7
Michael Glover <michigan.mm8@gmail.com>
2023-02-11 01:18:06 +0000

    ternary operator and coding challenge 4

---
+++ JavascriptFundamentals1/ternaryoperator.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const age = 18;
+// age >= 18
+//   ? console.log(`I like to drink wine`)
+//   : console.log(`I like to drink water`);
+
+const drink = age >= 18 ? `wine 🍷` : `water💧`;
+console.log(drink);
